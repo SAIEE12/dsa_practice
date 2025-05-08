48c1f6e1141491a32ccd9f1563fd6ddab3f5b4a6
Manish-A <saimanishsai19189@gmail.com>
2025-05-08 18:02:48 +0530

    practiced problems in the gfg

---
+++ day-2.py
@@ -0,0 +1,62 @@
+# Conditional Statements
+# Conditional statements are used to perform different actions based on different conditions. In Python, there are several conditional statements:
+# 1. if statement
+# 2. if-else statement
+# 3. if-elif-else statement
+# 4. nested if statement
+# 5. ternary operator (conditional expression)
+#
+# 1. if statement
+# The if statement is used to test a condition. If the condition is true, the block of code inside the if statement is executed.
+#
+# Example:
+# a = 10
+# if a > 5:
+#     print("a is greater than 5")
+#
+# 2. if-else statement
+# The if-else statement is used to test a condition. If the condition is true, the block of code inside the if statement is executed. If the condition is false, the block of code inside the else statement is executed.
+#
+# Example:          
+# a = 10
+# if a > 5:
+#     print("a is greater than 5")
+# else:
+#     print("a is not greater than 5")
+#
+# 3. if-elif-else statement
+# The if-elif-else statement is used to test multiple conditions. If the first condition is true, the block of code inside the if statement is executed. If the first condition is false, the next condition is tested, and so on. If none of the conditions are true, the block of code inside the else statement is executed.
+#
+# Example:
+# a = 10
+# if a > 10:                    
+#     print("a is greater than 10") 
+# elif a == 10:
+#     print("a is equal to 10")
+# else:
+#     print("a is less than 10")
+
+#
+# 4. nested if statement
+# The nested if statement is used to test a condition inside another if statement. If the first condition is true, the block of code inside the first if statement is executed. If the first condition is false, the block of code inside the else statement is executed.
+#
+# Example:
+# a = 10
+# if a > 5:
+#     print("a is greater than 5")
+#     if a > 10:
+#         print("a is greater than 10")
+#     else:
+#         print("a is not greater than 10")
+# else:
+#     print("a is not greater than 5")
+#
+# 5. ternary operator (conditional expression)
+# The ternary operator is a shorthand way of writing an if-else statement. It is used to evaluate a condition and return one of two values based on the result of the condition.
+#
+# Example:
+# a = 10
+# b = "a is greater than 5" if a > 5 else "a is not greater than 5"
+# print(b)
+#
+
